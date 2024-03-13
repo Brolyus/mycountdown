6d5785359df6a5c09a97cfe23007b0667c61e190
Brolyus <49352080+Brolyus@users.noreply.github.com>
2024-03-13 16:42:48 +0100

    finished

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        flicker:{
+          '0%, 19%, 21%, 23%, 25%, 54%, 56%, 100%': {
+            'text-shadow': '-0.2rem -0.2rem 1rem #fff, 0.2rem 0.2rem 1rem #fff, 0 0 2rem #f40, 0 0 4rem #f40,0 0 6rem #f40, 0 0 8rem #f40, 0 0 10rem #f40;',
+            'box-shadow': '0 0 .5rem #fff, inset 0 0 .5rem #fff, 0 0 2rem #08f, inset 0 0 2rem #08f, 0 0 4rem #08f,inset 0 0 4rem #08f;'
+          },
+          '20%, 24%, 55%': { 'text-shadow': 'none;', 'box-shadow': 'none;',}   
+        }
+      },
+      animation :{
+        'flicker':  'flicker 1.5s infinite alternate;'
+      }
     },
   },
   plugins: [],
